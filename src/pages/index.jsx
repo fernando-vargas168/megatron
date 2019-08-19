430e63e8942a93914f6e631516b6e313bce305e9
Kevin Kleber Rivamontan Alvarado <alejandrofaesta@gmail.com>
2019-08-19 12:47:27 -0400

    Formularios HoneyPot

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,62 @@ const index = () => {
   return (
     <div className="home">
       <Landing />
+      <form
+        hidden
+        action="/"
+        name="personas"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        netlify
+      >
+        <input type="hidden" name="form-name" value="personas" />
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="celular" name="email" />
+      </form>
+      <form
+        hidden
+        action="/"
+        name="cursos"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        netlify
+      >
+        <input type="hidden" name="form-name" value="cursos" />
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="celular" name="email" />
+      </form>
+      <form
+        hidden
+        action="/"
+        name="servicios"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        netlify
+      >
+        <input type="hidden" name="form-name" value="servicios" />
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="celular" name="email" />
+      </form>
+      <form
+        hidden
+        action="/"
+        name="empresas"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        netlify
+      >
+        <input type="hidden" name="form-name" value="empresas" />
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="celular" name="email" />
+      </form>
     </div>
   );
 };
